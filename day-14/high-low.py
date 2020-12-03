a3a6baf3b00f431f9b03b4f57d452299cc07e4e9
Terry Duong <tduong10101@gmail.com>
2020-12-03 22:21:00 +1100

    format code

--- day-14/high-low.py
+++ day-14/high-low.py
@@ -1,39 +1,49 @@
 from art import logo, vs
 from game_data import data
 import random
-from os import system,name
+from os import system, name
+
+
 def select_item(data):
     value = random.choice(data)
     data.remove(value)
-    return [value,data]
-def sideToString(side_variable,side):
+    return [value, data]
+
+
+def sideToString(side_variable, side):
     return f"Compare {side}: {side_variable['name']}, a {side_variable['description']}, from {side_variable['country']}"
-def clear():    
+
+
+def clear():
     if name == "nt":
         _ = system("cls")
     else:
         _ = system("clear")
     print(logo)
-def compare(sideA,sideB):
+
+
+def compare(sideA, sideB):
     if sideA['follower_count'] >= sideB['follower_count']:
         return True
     else:
         return False
+
+
 def game(data):
     print(logo)
-    sideA,data = select_item(data)
+    sideA, data = select_item(data)
     score = 0
     result = False
-    while len(data)>0:
-        sideB,data = select_item(data)
+    while len(data) > 0:
+        sideB, data = select_item(data)
         message = f"{sideToString(sideA,'A')}\n{vs}\n{sideToString(sideB,'B')}"
         print(message)
         anwser = (input("Who has more followers? Type 'A' or 'B': ")).lower()
         if anwser == "a":
-            result = compare(sideA,sideB)
+            result = compare(sideA, sideB)
             sideA = sideB
         elif anwser == "b":
-            result = compare(sideB,sideA)
+            result = compare(sideB, sideA)
         else:
             result = False
 
@@ -43,13 +53,13 @@ def game(data):
             score += 1
             clear()
             print(f"You're Right! Current score: {score}")
-            
 
     if result == False:
-        clear()        
+        clear()
         print(f"Sorry, that's wrong. Final score: {score}")
     else:
         clear()
         print(f"Congrats, you got them all correct! Final score: {score}")
-    
+
+
 game(data)
