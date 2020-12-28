c32105377976b3f73ff091ccacf43717f75b64fc
Terry Duong <tduong10101@gmail.com>
2020-12-28 23:05:11 +1100

    rename folder

--- day-26/NATO-alphabet-start/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-#load csv
-data = pd.read_csv("nato_phonetic_alphabet.csv")
-
-alphabet_dict = {row.letter:row.code for (index,row) in data.iterrows()}
-
-input_word = input("Enter a word: ")
-result = [alphabet_dict[letter] for letter in input_word.upper()]
-print(result)
